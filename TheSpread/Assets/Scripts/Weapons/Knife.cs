699b04690ec0fcb4e6d284e6a3af72dccf8f400d
Robert La Rocca <laroccrj@alfredstate.edu>
2013-11-12 11:16:38 -0500

    Added knife weapon

---
+++ TheSpread/Assets/Scripts/Weapons/Knife.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Knife : MonoBehaviour
+{
+	public float distance;
+	
+	void Update ()
+	{
+		if(Input.GetMouseButtonDown(0))
+		{
+			RaycastHit hit;
+			
+			if(Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, 2))
+			{
+				hit.collider.SendMessage("knifeHit", hit, SendMessageOptions.DontRequireReceiver);
+			}
+		}
+	}
+	
+}
--- TheSpread/Assets/Scripts/Zombie/Zombie.cs
+++ TheSpread/Assets/Scripts/Zombie/Zombie.cs
@@ -7,4 +7,14 @@ public class Zombie : MonoBehaviour {
 	{
 		Destroy(gameObject);	
 	}
+	
+	public void knifeHit(RaycastHit hit)
+	{
+		Vector3 contactPoint = transform.TransformPoint(hit.point);
+
+		if(contactPoint.z < 0)
+		{
+			this.Die();
+		}
+	}
 }
